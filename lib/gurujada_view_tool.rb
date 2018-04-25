71a7b057a7ff18f74e43d8e5708d11ad55de55fe
EC2 Default User <ec2-user@ip-172-31-30-67.us-east-2.compute.internal>
2018-04-25 05:45:54 +0000

    First commit

---
+++ lib/gurujada_view_tool.rb
@@ -0,0 +1,6 @@
+require "gurujada_view_tool/version"
+require "gurujada_view_tool/renderer"
+
+module GurujadaViewTool
+  # Your code goes here...
+end
---
+++ lib/gurujada_view_tool/version.rb
@@ -0,0 +1,3 @@
+module GurujadaViewTool
+  VERSION = "0.1.0"
+end
